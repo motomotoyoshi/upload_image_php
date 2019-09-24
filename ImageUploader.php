a048e94104cf45da2d9af29660d7e883f07f0828
motomotoyoshi <motomoto.git@gmail.com>
2019-09-24 10:49:16 +0900

    update, ImageUploader.php

--- ImageUploader.php
+++ ImageUploader.php
@@ -5,6 +5,29 @@ namespace MyApp;
 class ImageUploader {
 
     public function upload() {
-        
+        try {
+            $this->_validateUpload();
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            exit;
+        }
+        header('Location: http://' . $_SERVER['HTTP_HOST']);
+        exit;
+    }
+
+    private function _validateUpload() {
+        if (!isset($_FILES['image']) || !isset($_FILES['image']['error'])) {
+            throw new \Exception(('Upload Error'));
+        }
+
+        switch($_FILES['image']['error']) {
+            case UPLOAD_ERR_OK:
+                return true;
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                throw new \Exception('File too large');
+            default:
+                throw new \Exception('Err: ' . $_FILES['image']['error']);
+        }
     }
 }
